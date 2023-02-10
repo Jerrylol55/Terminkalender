be6d043a5c50027cda68be9a0add71c613e9c848
Jerrylol55 <jeremias.rieser@gmx.net>
2023-02-10 15:39:58 +0100

    new

---
+++ src/main/java/com/example/terminkalender/KalenderbeanV2.java
@@ -0,0 +1,11 @@
+package com.example.terminkalender;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class KalenderbeanV2 {
+
+
+}
---
+++ src/main/java/com/example/terminkalender/Termin.java
@@ -0,0 +1,17 @@
+package com.example.terminkalender;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Termin {
+
+     private Date Datum;
+     private String Von;
+     private String Bis;
+     private int Zimmer;
+     private String Bemerkung;
+     private String ListeVonTeilnehmer;
+
+
+
+}
